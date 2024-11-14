8c3816bd96e5fce31e7d8ed4c8b21da6451f139c
Nistha Verma <nisthav8@gmail.com>
2024-11-14 11:09:37 +0530

    green bg

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -119,11 +119,9 @@ function loadDelayed() {
   window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
 }
-
 async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
 }
-
 loadPage();
