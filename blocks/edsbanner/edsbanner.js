d24ba0efaeadfa0619705097f791517ae038fc96
Nistha Verma <nisthav8@gmail.com>
2024-11-15 13:03:59 +0530

    abc

--- blocks/edsbanner/edsbanner.js
+++ blocks/edsbanner/edsbanner.js
@@ -1,6 +1,3 @@
 export default function decorate(block) {
-    // const cols = [...block.firstElementChild.children];
-    // console.log(cols);
-    block.classList.add('eds-banner-container');
+  block.classList.add('eds-banner-container');
 }
-
