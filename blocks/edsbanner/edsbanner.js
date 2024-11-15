bd638029a6866fbb2cc5a64e87003166a011cf57
Nistha Verma <nisthav8@gmail.com>
2024-11-15 13:01:14 +0530

    changes made

--- blocks/edsbanner/edsbanner.js
+++ blocks/edsbanner/edsbanner.js
@@ -1,4 +1,6 @@
-export default function decorate(block){
-    console.log(block);
-block.classList.add('eds-banner-container');
+export default function decorate(block) {
+    // const cols = [...block.firstElementChild.children];
+    // console.log(cols);
+    block.classList.add('eds-banner-container');
 }
+
