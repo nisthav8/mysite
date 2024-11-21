fdf087ea25a898f6a2f02878914b325dc6d07e1e
Nistha Verma <nisthav8@gmail.com>
2024-11-21 12:33:46 +0530

    header cleared

--- blocks/eds-banner/eds-banner.js
+++ blocks/eds-banner/eds-banner.js
@@ -1,19 +1,19 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+// import { getMetadata } from '../../scripts/aem.js';
+// import { loadFragment } from '../fragment/fragment.js';
 
-export default async function decorate(block) {
-  // load banner as fragment
-  const bannerMeta = getMetadata('eds-banner');
-  // console.log("a"+bannerMeta);
-  const bannerPath = bannerMeta ? new URL(bannerMeta, window.location).pathname : '/eds-banner';
-  // console.log("b"+bannerPath);
-  const fragment = await loadFragment(bannerPath);
-  // console.log("c"+fragment)
+// export default async function decorate(block) {
+//   // load banner as fragment
+//   const bannerMeta = getMetadata('eds-banner');
+//   // console.log("a"+bannerMeta);
+//   const bannerPath = bannerMeta ? new URL(bannerMeta, window.location).pathname : '/eds-banner';
+//   // console.log("b"+bannerPath);
+//   const fragment = await loadFragment(bannerPath);
+//   // console.log("c"+fragment)
 
-  // decorate banner DOM
-  block.textContent = '';
-  const banner = document.createElement('div');
-  while (fragment.firstElementChild) banner.append(fragment.firstElementChild);
-  // console.log("d"+banner)
-  block.append(banner);
-}
+//   // decorate banner DOM
+//   block.textContent = '';
+//   const banner = document.createElement('div');
+//   while (fragment.firstElementChild) banner.append(fragment.firstElementChild);
+//   // console.log("d"+banner)
+//   block.append(banner);
+// }
